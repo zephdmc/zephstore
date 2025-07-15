57751bed3cff99bff2a167806de0aee3c251f9f6
zephdmc <zephdmc@gmail.com>
2025-07-15 19:31:44 +0100

    Update Register.jsx
    sl

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {
     createUserWithEmailAndPassword,
