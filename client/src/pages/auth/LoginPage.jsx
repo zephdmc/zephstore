794781b93922820171688ac2d774b4e321163fe9
zephdmc <zephdmc@gmail.com>
2025-07-16 18:28:02 +0100

    Update LoginPage.jsx
    color to light

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -2,7 +2,7 @@ import Login from '../../components/auth/Login';
 
 export default function LoginPage() {
     return (
-        <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-2 md:py-12  sm:px-6 lg:px-8">
+        <div className="min-h-screen bg-purpleLight flex flex-col justify-center py-2 md:py-12  sm:px-6 lg:px-8">
 
             <div className="mt-2 md:mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="bg-white py-2 md:py-8 px-4 shadow sm:rounded-lg sm:px-10">
