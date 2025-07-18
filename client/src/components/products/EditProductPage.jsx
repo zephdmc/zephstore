9f4753654c6f063c84c6c3ce77b5c6c66eb2b6f6
zephdmc <zephdmc@gmail.com>
2025-07-18 08:12:19 -0700

    Update EditProductPage.jsx
    added color to edit button

--- client/src/components/products/EditProductPage.jsx
+++ client/src/components/products/EditProductPage.jsx
@@ -352,7 +352,7 @@ export default function EditProductPage() {
                     <button
                         type="submit"
                         disabled={loading}
-                        className="px-4 py-2 border border-transparent rounded-md shadow-sm text-xs sm:text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed"
+                        className="px-4 py-2 border border-transparent rounded-md shadow-sm text-xs sm:text-sm font-medium text-white bg-purplegradient hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed"
                     >
                         {loading ? (
                             <span className="flex items-center justify-center">
