98d89d2bc63995f8cebb0d3f7ac0c8689e78a151
zephdmc <zephdmc@gmail.com>
2025-07-18 05:57:04 -0700

    Update RegisterPage.jsx
    added z-10

--- client/src/pages/auth/RegisterPage.jsx
+++ client/src/pages/auth/RegisterPage.jsx
@@ -17,7 +17,7 @@ export default function RegisterPage() {
                     Create a new account
                 </h2> */}
             </div>
-            <div className="mt-2 md:mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="mt-2 md:mt-8 sm:mx-auto z-20 sm:w-full sm:max-w-md">
                 <div className="bg-white py-2 md:py-8 px-4 shadow sm:rounded-lg sm:px-10">
                     <Register />
                 </div>
