07a5ede93ec2f1f5b748338fce7bc9a0387a210d
zephdmc <zephdmc@gmail.com>
2025-07-18 05:53:51 -0700

    Update RegisterPage.jsx
    background image

--- client/src/pages/auth/RegisterPage.jsx
+++ client/src/pages/auth/RegisterPage.jsx
@@ -3,6 +3,15 @@ import Register from '../../components/auth/Register';
 export default function RegisterPage() {
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+              {/* Background Image with Opacity */}
+    <div className="absolute inset-0 z-0">
+        <img 
+            src="/images/hero1.jpeg" // Replace with your image path
+            alt="Beauty Products Background"
+            className="w-full h-full object-cover opacity-20" // Adjust opacity as needed (0.2 = 20%)
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-purpleDark to-purpleLight mix-blend-multiply"></div>
+    </div>
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 {/* <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                     Create a new account
