98b6012c897b011acd5c32f4375519416b8c8a7e
zephdmc <zephdmc@gmail.com>
2025-07-22 03:26:24 -0700

    Create contentful.js
    intigrating contentfull API

---
+++ client/src/services/contentful.js
@@ -0,0 +1,8 @@
+import { createClient } from 'contentful';
+
+const client = createClient({
+  space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
+});
+
+export default client;
