2f44377f939a752b8cac2099862f56eedd623211
zephdmc <zephdmc@gmail.com>
2025-07-25 02:13:14 -0700

    Update SkincareQuizForm.js
    sdrfre

--- client/src/pages/home/HomePageComponent/SkincareQuizForm.js
+++ client/src/pages/home/HomePageComponent/SkincareQuizForm.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
 
   try {
     const response = await fetch(
-      'https://script.google.com/macros/s/AKfycbwmurUvQstoAFxG1KmgA0ZyqbyInhbrYPSFeRXK8z2pA9PTXvcpd_n_-AQlJsVR4NQmiw/exec',
+      'https://script.google.com/macros/s/AKfycbxOUnwRbXtdBJyMTisVRDvUZNPQoyuo4WUYJxjo_eDW9wgd3V2idabtCncsAm8DpWbztA/exec',
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
