bfa049ee166b1d5463db974e76d7686d632ddc38
zephdmc <zephdmc@gmail.com>
2025-07-25 04:53:02 +0100

    Update HomePage.jsx
    sizing

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -321,16 +321,16 @@ useEffect(() => {
                                 {product.discountPercentage > 0 ? (
                                     <>
                                         <div className="flex items-center gap-2">
-                                            <span className="text-sm md:text-xl text-white line-through">
+                                            <span className="text-[6px] md:text-lg text-white line-through">
                                                 ₦{(product.price + (product.price * (product.discountPercentage / 100))).toFixed(2)}
                                             </span>
-                                            <span className="text-sm md:text-xl text-purpleDark1 font-bold">
+                                            <span className="text-[6px] md:text-lg text-purpleDark1 font-bold">
                                                 ₦{product.price.toFixed(2)}
                                             </span>
                                         </div>
                                     </>
                                 ) : (
-                                    <span className="text-sm md:text-xl text-white">
+                                    <span className="text-[6px] md:text-xl text-white">
                                         ₦{product.price.toFixed(2)}
                                     </span>
                                 )}
