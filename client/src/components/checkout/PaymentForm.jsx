ac203b50a9d0c66c2d48a78113c02c0abee250e1
zephdmc <zephdmc@gmail.com>
2025-08-02 12:33:54 +0100

    Update PaymentForm.jsx
       FF

--- client/src/components/checkout/PaymentForm.jsx
+++ client/src/components/checkout/PaymentForm.jsx
@@ -153,7 +153,7 @@ const metaPayload = {
                     amount,
                     meta: {
                         userId,
-                        items: cartItems.map(i => i?.id).filter(Boolean),
+                        //items: cartItems.map(i => i?.id).filter(Boolean),
                         txRef,
                         securityToken
                     }
