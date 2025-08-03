d3e5cf7b228fde8cc1f2e6a99a6c3362ef2be9e6
zephdmc <zephdmc@gmail.com>
2025-08-03 12:22:25 +0100

    Update OrderConfirmation.jsx
    now

--- client/src/components/checkout/OrderConfirmation.jsx
+++ client/src/components/checkout/OrderConfirmation.jsx
@@ -47,7 +47,7 @@ const OrderConfirmation = ({ order }) => {
                     to={`/orders/${order.data.id}`}
                     className="bg-primary text-purpleDark py-2 px-6 rounded hover:bg-purpleDark1 transition font-medium"
                 >
-                    View Order Details
+                    View Order Details Now
                 </Link>
                 <Link
                     to="/products"
