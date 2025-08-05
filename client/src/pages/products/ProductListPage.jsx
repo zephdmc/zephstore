508ffc12ff14f21a5f1041eb9ac52b98b35f575b
zephdmc <zephdmc@gmail.com>
2025-08-05 14:37:09 +0100

    Update ProductListPage.jsx
    all product text whote

--- client/src/pages/products/ProductListPage.jsx
+++ client/src/pages/products/ProductListPage.jsx
@@ -56,7 +56,7 @@ export default function ProductListPage() {
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-2xl font-bold mb-6">All Products</h1>
+            <h1 className="text-2xl text-white font-bold mb-6">All Products</h1>
 
             <ProductFilter categories={categories} onFilter={handleFilter} />
 
