797c7ae687601774ec29ce31599f606235b91e20
zephdmc <zephdmc@gmail.com>
2025-08-05 15:22:58 +0100

    Update CartPage.jsx
    ce

--- client/src/pages/cart/CartPage.jsx
+++ client/src/pages/cart/CartPage.jsx
@@ -23,7 +23,7 @@ export default function CartPage() {
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-2xl font-bold mb-6">Shopping Cart</h1>
+            <h1 className="text-2xl text-purpleDark1 font-bold mb-6">Shopping Cart</h1>
 
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
                 <div className="lg:col-span-2 bg-white rounded-lg shadow">
